fbd6cb61ba6b5802b92f6f99fd2494dde60acad1
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2024-07-05 00:31:04 +0100

    trying to fix linker error

--- src/main.cpp
+++ src/main.cpp
@@ -2,33 +2,37 @@
 #include <config.h>
 #endif
 
+#include "../lib/TSQueue.h"
 #include <cstdlib>
 #include <iostream>
 #include <libxml++/libxml++.h>
 #include <libxml++/parsers/textreader.h>
-#include <mutex>
-#include <queue>
+#include <stdexcept>
 
 int main(int argc, char *argv[]) {
     try {
         std::string filepath;
+
         if (argc > 1) {
             filepath = argv[1];
-
         } else {
-            filepath = "example.xml";
+            throw std::invalid_argument("No file provided");
         }
+
+        TSQueue<int> q;
+
+        q.push(5);
         xmlpp::TextReader reader(filepath);
         while (reader.read()) {
-            int depth = reader.get_depth();
-            if (reader.get_name() == "page") {
-
-                std::cout << "--- node ---" << std::endl;
-
-                std::cout << reader.read_outer_xml() << std::endl;
+            std::string name = reader.get_name();
+            if (name == "page") {
+                std::cout << q.pop() << std::endl;
+                std::string output(reader.read_outer_xml());
+                std::cout << "!Name: " << output << std::endl;
+            } else {
+                std::cout << "Name: " << name << std::endl;
             }
         }
-
     } catch (const std::exception &e) {
         std::cerr << "Exception caught: " << e.what() << std::endl;
         return EXIT_FAILURE;
