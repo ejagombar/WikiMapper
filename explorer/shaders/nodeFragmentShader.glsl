f44a9257de21b3724585ad9fe59f11b43ef9deff
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2024-08-05 22:52:06 +0100

    got spheres floating in space wohooo

--- explorer/shaders/nodeFragmentShader.glsl
+++ explorer/shaders/nodeFragmentShader.glsl
@@ -1,8 +1,9 @@
 #version 330 core
 
+#extension GL_ARB_explicit_uniform_location : require
 // Interpolated values from the vertex shaders
 in vec2 UV;
-in vec4 particlecolor;
+in vec4 nodecolor;
 
 // Output data
 out vec4 color;
@@ -11,5 +12,5 @@ uniform sampler2D myTextureSampler;
 
 void main() {
     // Output color = color of the texture at the specified UV
-    color = texture(myTextureSampler, UV) * particlecolor;
+    color = texture(myTextureSampler, UV) * nodecolor;
 }
--- explorer/shaders/nodeVertexShader.glsl
+++ explorer/shaders/nodeVertexShader.glsl
@@ -1,5 +1,7 @@
 #version 330 core
 
+#extension GL_ARB_explicit_uniform_location : require
+
 // Input vertex data, different for all executions of this shader.
 layout(location = 0) in vec3 squareVertices;
 layout(location = 1) in vec4 xyzs; // Position of the center of the particle and size of the square
@@ -7,7 +9,7 @@ layout(location = 2) in vec4 color; // Position of the center of the particle an
 
 // Output data ; will be interpolated for each fragment.
 out vec2 UV;
-out vec4 particlecolor;
+out vec4 nodecolor;
 
 // Values that stay constant for the whole mesh.
 uniform vec3 CameraRight_worldspace;
@@ -16,18 +18,18 @@ uniform mat4 VP; // Model-View-Projection matrix, but without the Model (the pos
 
 void main()
 {
-    float particleSize = xyzs.w; // because we encoded it this way.
-    vec3 particleCenter_wordspace = xyzs.xyz;
+    float nodeSize = xyzs.w; // because we encoded it this way.
+    vec3 nodeCenter_wordspace = xyzs.xyz;
 
     vec3 vertexPosition_worldspace =
         particleCenter_wordspace
-            + CameraRight_worldspace * squareVertices.x * particleSize
-            + CameraUp_worldspace * squareVertices.y * particleSize;
+            + CameraRight_worldspace * squareVertices.x * nodeSize
+            + CameraUp_worldspace * squareVertices.y * nodeSize;
 
     // Output position of the vertex
     gl_Position = VP * vec4(vertexPosition_worldspace, 1.0f);
 
     // UV of the vertex. No special space for this one.
     UV = squareVertices.xy + vec2(0.5, 0.5);
-    particlecolor = color;
+    nodecolor = color;
 }
