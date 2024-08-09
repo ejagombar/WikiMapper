6cc29a1b712d958982d25cb24e61cbccb3ad6fba
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2024-08-09 20:07:35 +0100

    remove verbose comments from tutorial

--- explorer/src/gui.cpp
+++ explorer/src/gui.cpp
@@ -75,7 +75,6 @@ void gui::generateNodeData(Node *NodeContainer, int size) {
 }
 
 void gui::loop() {
-    // Clear the screen
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
     double currentTime = glfwGetTime();
@@ -113,32 +112,24 @@ void gui::loop() {
     }
 
     glBindBuffer(GL_ARRAY_BUFFER, node_position_buffer);
-    glBufferData(GL_ARRAY_BUFFER, m_MaxNodes * 4 * sizeof(GLfloat), NULL,
-                 GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf.
-                                  // See above link for details.
+    glBufferData(GL_ARRAY_BUFFER, m_MaxNodes * 4 * sizeof(GLfloat), NULL, GL_STREAM_DRAW);
     glBufferSubData(GL_ARRAY_BUFFER, 0, NodeCount * sizeof(GLfloat) * 4, g_node_position_size_data);
 
     glBindBuffer(GL_ARRAY_BUFFER, node_color_buffer);
-    glBufferData(GL_ARRAY_BUFFER, m_MaxNodes * 4 * sizeof(GLubyte), NULL,
-                 GL_STREAM_DRAW); // Buffer orphaning, a common way to improve streaming perf.
-                                  // See above link for detai#extension
-                                  // GL_ARB_explicit_uniform_location : requirels.
+    glBufferData(GL_ARRAY_BUFFER, m_MaxNodes * 4 * sizeof(GLubyte), NULL, GL_STREAM_DRAW);
     glBufferSubData(GL_ARRAY_BUFFER, 0, NodeCount * sizeof(GLubyte) * 4, g_node_color_data);
 
     glEnable(GL_BLEND);
     glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
     glEnable(GL_LINE_SMOOTH);
 
-    // Use our shader
     glUseProgram(programID);
 
-    // Bind our texture in Texture Unit 0
     glActiveTexture(GL_TEXTURE0);
     glBindTexture(GL_TEXTURE_2D, Texture);
-    // Set our "myTextureSampler" sampler to use Texture Unit 0
+
     glUniform1i(TextureID, 0);
 
-    // Same as the billboards tutorial
     glUniform3f(CameraRight_worldspace_ID, ViewMatrix[0][0], ViewMatrix[1][0], ViewMatrix[2][0]);
     glUniform3f(CameraUp_worldspace_ID, ViewMatrix[0][1], ViewMatrix[1][1], ViewMatrix[2][1]);
     vec3 lightPos = CameraPosition;
@@ -155,64 +146,32 @@ void gui::loop() {
 
     glUniformMatrix4fv(ViewProjMatrixID, 1, GL_FALSE, &ViewProjectionMatrix[0][0]);
 
-    // 1rst attribute buffer : vertices
+    // 1st attribute buffer : vertices
     glEnableVertexAttribArray(0);
     glBindBuffer(GL_ARRAY_BUFFER, billboard_vertex_buffer);
-    glVertexAttribPointer(
-        0,        // attribute. No particular reason for 0, but must match the layout in the shader.
-        3,        // size
-        GL_FLOAT, // type
-        GL_FALSE, // normalized?
-        0,        // stride
-        (void *)0 // array buffer offset
-    );
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, (void *)0);
 
     // 2nd attribute buffer : positions of particles' centers
     glEnableVertexAttribArray(1);
     glBindBuffer(GL_ARRAY_BUFFER, node_position_buffer);
-    glVertexAttribPointer(
-        1,        // attribute. No particular reason for 1, but must match the layout in the shader.
-        4,        // size : x + y + z + size => 4
-        GL_FLOAT, // type
-        GL_FALSE, // normalized?#extension GL_ARB_explicit_uniform_location : require
-        0,        // stride
-        (void *)0 // array buffer offset
-    );
+    glVertexAttribPointer(1, 4, GL_FLOAT, GL_FALSE, 0, (void *)0);
 
     // 3rd attribute buffer : particles' colors
     glEnableVertexAttribArray(2);
     glBindBuffer(GL_ARRAY_BUFFER, node_color_buffer);
-    glVertexAttribPointer(
-        2, // attribute. No particular reason for 1, but must match the layout in the shader.
-        4, // size : r + g + b + a => 4
-        GL_UNSIGNED_BYTE, // type
-        GL_TRUE,          // normalized?    *** YES, this means that the unsigned char[4] will be
-                          // accessible with a vec4 (floats) in the shader ***
-        0,                // stride
-        (void *)0         // array buffer offset
-    );
+    glVertexAttribPointer(2, 4, GL_UNSIGNED_BYTE, GL_TRUE, 0, (void *)0);
 
-    // These functions are specific to glDrawArrays*Instanced*.
-    // The first parameter is the attribute buffer we're talking about.
-    // The second parameter is the "rate at which generic vertex attributes advance when
-    // rendering multiple instances"
-    // http://www.opengl.org/sdk/docs/man/xhtml/glVertexAttribDivisor.xml
     glVertexAttribDivisor(0, 0); // particles vertices : always reuse the same 4 vertices -> 0
     glVertexAttribDivisor(1, 1); // positions : one per quad (its center)                 -> 1
     glVertexAttribDivisor(2, 1); // color : one per quad                                  -> 1
 
-    // Draw the
-    // This draws many times a small triangle_strip (which looks like a quad).
-    // This is equivalent to :
-    // for(i in NodeCount) : glDrawArrays(GL_TRIANGLE_STRIP, 0, 4),
-    // but faster.
     glDrawArraysInstanced(GL_TRIANGLE_STRIP, 0, 4, NodeCount);
 
     glDisableVertexAttribArray(0);
     glDisableVertexAttribArray(1);
     glDisableVertexAttribArray(2);
 
-    // // ---------------------------- LINES ----------------------------
+    // ---------------------------- LINES ----------------------------
     glUseProgram(shaderProgram);
 
     GLuint VP2 = glGetUniformLocation(shaderProgram, "VP");
