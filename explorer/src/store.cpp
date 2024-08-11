d854ec992a3302123ff36de058c91c7cdfe971dd
Edward Agombar <77460324+ejagombar@users.noreply.github.com>
2024-08-11 13:17:53 +0100

    biniomial distribution of the number of links

--- explorer/src/store.cpp
+++ explorer/src/store.cpp
@@ -1,15 +1,22 @@
 #include "store.h"
+#include <random>
 
 void generateFakeData(DB &db, const int size) {
+
     const int idOffset = 1000;
     db.resize(size);
+
+    std::default_random_engine generator;
+    std::binomial_distribution<int> distribution(25, 0.1);
 
     NodeStore obj;
     for (int id = 0; id < size; ++id) {
         obj.UID = id + idOffset;
         obj.name = "Node" + std::to_string(obj.UID);
 
-        int numOfLinks = 1 + rand() % 4;
+        // int numOfLinks = 1 + rand() % 4;
+        int numOfLinks = distribution(generator);
+
         obj.linksTo.resize(numOfLinks);
 
         for (int i = 0; i < numOfLinks; i++) {
